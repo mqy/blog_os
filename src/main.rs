950086b573b4d6bc2a725f9af4f26017d2baf0ac
mqy <meng.qingyou@gmail.com>
2022-02-23 16:48:39 +0800

    add .cargo; emit asm

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,12 @@
 #![no_std]
 #![no_main]
 
+// build for arm: cargo build --target thumbv7em-none-eabihf
+
+// build in Linux/MacOs/Windows (see .cargo/config.toml): cargo build
+
+// emit asm: rustc src/main.rs --emit asm --target thumbv7em-none-eabihf
+
 use core::panic::PanicInfo;
 
 #[no_mangle]
